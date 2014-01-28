12cbdc42c950bfeb8a859409cb1fd4ec0cf32916
kylinmod <kylinmod@gmail.com>
2014-01-28 18:19:29 +0800

    Kylin Modify

--- kylin.mk
+++ kylin.mk
@@ -1,13 +1,13 @@
 # Inherit device configuration for VZW Droid RAZR.
 $(call inherit-product, device/motorola/umts_spyder/full_umts_spyder.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common KM stuff.
+$(call inherit-product, vendor/kylin/config/common_full_phone.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := cm_umts_spyder
+PRODUCT_NAME := km_umts_spyder
 PRODUCT_BRAND := RTGB
 PRODUCT_DEVICE := umts_spyder
 PRODUCT_MODEL := XT910
@@ -27,4 +27,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
    PRODUCT_BRAND=${PRODUCT_BRAND} \
    BUILD_UTC_DATE= \
    PRODUCT_DEFAULT_LANGUAGE=en \
-   PRODUCT_DEFAULT_REGION=US \
+   PRODUCT_DEFAULT_REGION=US
